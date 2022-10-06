85ed901ff19e09d33f316d0570edd7b45242fe0b
pseudopulse <psuedopulse@protonmail.com>
2022-10-05 22:46:19 -0500

    changed bottled meta to use a monobehavior so it doesnt have 2423 bugs

--- GOTCE/Items/BottledMetamorphosis.cs
+++ GOTCE/Items/BottledMetamorphosis.cs
@@ -27,9 +27,6 @@ namespace GOTCE.Items
 
         public override Sprite ItemIcon => null;
 
-
-        private float stopwatch = 5f;
-        private float interval = 5f;
         
 
         private static readonly System.Random random = new System.Random();
@@ -54,12 +51,14 @@ namespace GOTCE.Items
 
         public override void Hooks()
         {
-            On.RoR2.CharacterBody.FixedUpdate += Transform;
-            On.RoR2.CharacterBody.OnInventoryChanged += StackTimer;
+            // On.RoR2.CharacterBody.FixedUpdate += Transform;
+            // On.RoR2.CharacterBody.OnInventoryChanged += StackTimer;
+            // On.RoR2.Stage.FixedUpdate += UpdateTimer; 
+            On.RoR2.CharacterBody.OnInventoryChanged += AttachController;
         }
         
 
-        public GameObject GetRandomCharacterBodyPrefab() {
+        public static GameObject GetRandomCharacterBodyPrefab() {
             List<string> donot = new List<string>() {
                 "BirdsharkBody", "ArtifactShellBody","AltarSkeletonBody", "BackupDroneOldBody", "BeetleCrystalBody", "BeetleGuardAllyBody", "BeetleGuardCrystalBody",
             "BeetleWard", "DeathProjectile", "ExplosivePotDestructibleBody", "FusionCellDestructibleBody", "GolemBodyInvincible",
@@ -78,29 +77,80 @@ namespace GOTCE.Items
             return bodies[random.Next(0, bodies.Count)];
         }
 
-        public void Transform(On.RoR2.CharacterBody.orig_FixedUpdate orig, CharacterBody self) {
+        /* public void Transform(On.RoR2.CharacterBody.orig_FixedUpdate orig, CharacterBody self) {
             orig(self);
-            stopwatch -= Time.fixedDeltaTime;
             if (self.inventory) {
                 if (self.inventory.GetItemCount(ItemDef) > 0) {
                     // Main.ModLogger.LogDebug(stopwatch);
                     if (stopwatch <= 0) {
                         self.master.bodyPrefab = GetRandomCharacterBodyPrefab();
-                        // Main.ModLogger.LogDebug(self.master.bodyPrefab.name);
                         self.master.Respawn(self.master.GetBody().transform.position, self.master.GetBody().transform.rotation);
                         // self.AddTimedBuff(MetamorphoTimer.Buff, 5f);
-                        stopwatch = interval;
+                    }
+                }
+            }
+        } */
+
+        public void StackTimer(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self) {
+            orig(self);
+            if (self.inventory && self.inventory.GetItemCount(ItemDef) > 0) {
+                // interval = 5f * Mathf.Pow(0.9f, self.inventory.GetItemCount(ItemDef));
+                if (self.gameObject.GetComponent<MetaController>()) {
+                    // self.gameObject.GetComponent<MetaController>().interval =  5f * Mathf.Pow(0.9f, self.inventory.GetItemCount(ItemDef));
+                }
+            }
+        }
+
+        public void AttachController(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self) {
+            orig(self);
+            if (self.inventory) {
+                bool flag = self.inventory.GetItemCount(ItemDef) > 0;
+                MetaController controller = self.GetComponent<MetaController>();
+                if (flag != controller) {
+                    if (flag) {
+                        self.gameObject.AddComponent<MetaController>();
+                    }
+                    else {
+                        Object.Destroy(controller);
                     }
                 }
             }
         }
 
-        public void StackTimer(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, CharacterBody self) {
-            if (self.inventory && self.inventory.GetItemCount(ItemDef) > 0) {
-                interval = 5f * Mathf.Pow(0.9f, self.inventory.GetItemCount(ItemDef));
+        /* public void UpdateTimer(On.RoR2.Stage.orig_FixedUpdate orig, Stage self) {
+            orig(self);
+            stopwatch -= Time.fixedDeltaTime;
+            if (stopwatch <= 0) {
+                stopwatch = 5f;
             }
+        } */
+
+    }
+
+    public class MetaController : MonoBehaviour {
+        private CharacterBody body;
+        private static float interval = 5f;
+        private float stopwatch = interval;
+
+
+        public void Start() {
+            body = gameObject.GetComponent<CharacterBody>();
+            interval = 5f * Mathf.Pow(0.9f, body.inventory.GetItemCount(BottledMetamorphosis.Instance.ItemDef));
         }
 
+        public void FixedUpdate() {
+            stopwatch -= Time.fixedDeltaTime;
+            if (body.inventory) {
+                if (body.inventory.GetItemCount(BottledMetamorphosis.Instance.ItemDef) > 0) {
+                    Main.ModLogger.LogDebug(stopwatch);
+                    if (stopwatch <= 0) {
+                        body.master.bodyPrefab = BottledMetamorphosis.GetRandomCharacterBodyPrefab();
+                        body.master.Respawn(body.master.GetBody().transform.position, body.master.GetBody().transform.rotation);
+                        // self.AddTimedBuff(MetamorphoTimer.Buff, 5f);
+                    }
+                }
+            }
+        }
     }
 
 
