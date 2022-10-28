eb8e790dd29cc15e3786e8ba4c3c86061b5f4deb
MonsterSkinMan <matthewebb6305@gmail.com>
2022-10-28 18:57:32 -0400

    boxing gloves and clasping claws

---
+++ GOTCE/Items/Void Green/ClaspingClaws.cs
@@ -0,0 +1,71 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GOTCE.Items.VoidGreen
+{
+    public class ClaspingClaws : ItemBase<ClaspingClaws>
+    {
+        public override string ConfigName => "Clasping Claws";
+
+        public override string ItemName => "Clasping Claws";
+
+        public override string ItemLangTokenName => "GOTCE_ClaspingClaws";
+
+        public override string ItemPickupDesc => "Pull enemies towards you on hit";
+
+        public override string ItemFullDescription => "100% chance on hit to pull enemies towards you, increasing in force per stack. Corrupts all Boxing Gloves.";
+
+        public override string ItemLore => "TBA";
+
+        public override ItemTier Tier => ItemTier.VoidTier2;
+
+        public override ItemTag[] ItemTags => new ItemTag[] { ItemTag.Utility };
+
+        public override GameObject ItemModel => null;
+
+        public override Sprite ItemIcon => null;
+
+        public override void Init(ConfigFile config)
+        {
+            base.Init(config);
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict(null);
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.HealthComponent.TakeDamage += Smash4PacManGrabIsSoAwesomeBro;
+        }
+
+        private void Smash4PacManGrabIsSoAwesomeBro(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
+            if (damageInfo?.attacker)
+            {
+                var ThePacIsBack = damageInfo.attacker.GetComponent<RoR2.CharacterBody>();
+                if (ThePacIsBack)
+                {
+                    int stack = GetCount(ThePacIsBack);
+                    if (stack > 0)
+                    {
+                        float mass;
+                        if (self.body.characterMotor) mass = (self.body.characterMotor as IPhysMotor).mass;
+                        else if (self.body.rigidbody) mass = self.body.rigidbody.mass;
+                        else mass = 1f;
+
+                        var DahRoFus = -1500f - (750f * (stack - 1));
+                        damageInfo.force += Vector3.Normalize(self.body.corePosition - ThePacIsBack.corePosition) * DahRoFus * mass;
+                    }
+                }
+            }
+            orig(self, damageInfo);
+        }
+    }
+}
