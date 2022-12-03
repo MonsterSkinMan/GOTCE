79b5ddd937dd8ffad72b3939fae5864548915b3f
MonsterSkinMan <matthewebb6305@gmail.com>
2022-12-03 18:20:54 -0500

    cracked vermin and clasping claws icons

--- GOTCE/Items/Void Green/ClaspingClaws.cs
+++ GOTCE/Items/Void Green/ClaspingClaws.cs
@@ -28,7 +28,7 @@ namespace GOTCE.Items.VoidGreen
 
         public override GameObject ItemModel => null;
 
-        public override Sprite ItemIcon => null;
+        public override Sprite ItemIcon => Main.MainAssets.LoadAsset<Sprite>("Assets/Textures/Icons/Item/ClaspingClaws.png");
 
         public override void Init(ConfigFile config)
         {
