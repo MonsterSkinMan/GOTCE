afd34f5897a3e9b84d4183230c9010ad9caa8c22
pseudopulse <psuedopulse@protonmail.com>
2023-01-14 14:04:17 -0600

    real 3

--- GOTCE/Mechanics/WarCrimes.cs
+++ GOTCE/Mechanics/WarCrimes.cs
@@ -110,7 +110,7 @@ namespace GOTCE.Mechanics
             };
 
             // killing a mending core
-            /*On.RoR2.GlobalEventManager.OnCharacterDeath += (orig, self, report) =>
+            On.RoR2.GlobalEventManager.OnCharacterDeath += (orig, self, report) =>
             {
                 if (NetworkServer.active && report.victimBody && report.victimBody.baseNameToken == Utils.Paths.GameObject.AffixEarthHealerBody.LoadComponent<CharacterBody>().baseNameToken && NetworkServer.active)
                 {
@@ -120,7 +120,7 @@ namespace GOTCE.Mechanics
                     }
                 }
                 orig(self, report);
-            }; */
+            };
 
             // healing bonus
             On.RoR2.HealthComponent.Heal += (orig, self, amount, mask, boolv) =>
