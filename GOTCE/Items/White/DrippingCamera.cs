baaf8d7d331c128032ec2d61e378208f925005ed
MonsterSkinMan <matthewebb6305@gmail.com>
2023-07-18 23:29:54 -0400

    ok no this is the realest I swear

--- GOTCE/Items/White/DrippingCamera.cs
+++ GOTCE/Items/White/DrippingCamera.cs
@@ -18,7 +18,7 @@ namespace GOTCE.Items.White
 
         public override string ItemFullDescription => "Every second, you have a <style=cIsUtility>10%</style> <style=cStack>(+10% chance per stack)</style> to <style=cIsUtility>'Camera Rotation Crit'</style>, <style=cIsDamage>rapidly spinning your vision</style>.";
 
-        public override string ItemLore => "";
+        public override string ItemLore => "Expedition log - M. Johnson\nDay 4\n\nApproaching the subject now appears possible, as long as it is not observed on the way there. Unfortunately it gets a bit tricky then, so I will describe what happens with my interactions.\n\nI look at the subject, I turn away, I turn back, not looking, look at the subject again, I turn away. I point the camera at it, the camera drops into the swamp water. I bend down to pick it up, gazing over the subject, and turn away. I turn back around facing down and retrieve the camera. I close my eyes and look up, I immediately turn away and drop my camera. At least it's waterproof. I glance at the subject reflected off my camera’s screen, and immediately turn around, walk a few steps, and drop my camera again.\n\nI still have no idea what this thing even looks like.";
 
         public override ItemTier Tier => ItemTier.Tier1;
 
