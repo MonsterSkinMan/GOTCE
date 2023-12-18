33aae3c91caf6448490451e6c961794c67948926
MonsterSkinMan <matthewebb6305@gmail.com>
2023-12-18 15:43:37 -0500

    rushing elite, squid ink spaghetti, vessel of heresy, blue whip, updated doom blast icon

---
+++ GOTCE/Equipment/EliteEquipment/Rushing.cs
@@ -0,0 +1,99 @@
+﻿using BepInEx.Configuration;
+using R2API;
+using RoR2;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using RoR2.Skills;
+
+namespace GOTCE.Equipment.EliteEquipment
+{
+    class Rushing : EliteEquipmentBase<Rushing>
+    {
+        public override string EliteEquipmentName => "Blessing of Woolie";
+
+        public override string EliteAffixToken => "RUSH";
+
+        public override string EliteEquipmentPickupDesc => "Become an aspect of speed.";
+
+        public override string EliteEquipmentFullDescription => "";
+
+        public override string EliteEquipmentLore => "";
+        public override Texture2D EliteRampTexture => Main.SecondaryAssets.LoadAsset<Texture2D>("Assets/Prefabs/EliteOverlays/Backup.png"); //Change this to rushing elite ramp at some point (should be like a yellow color or smth)
+
+        public override string EliteModifier => "Rushing";
+
+        public override GameObject EliteEquipmentModel => Main.SecondaryAssets.LoadAsset<GameObject>("Assets/Prefabs/Equipment/Backup/BackupAspect.prefab"); //Again, change to rushing when real
+
+        public override Sprite EliteEquipmentIcon => Main.SecondaryAssets.LoadAsset<Sprite>("Assets/Prefabs/Equipment/Backup/BackupIcon.png"); //You know the drill
+        public override Sprite EliteBuffIcon => Main.SecondaryAssets.LoadAsset<Sprite>("Assets/Icons/Buffs/BackupAffixIcon.png"); //gd1
+        public override float DamageMultiplier => 2f;
+        public override float HealthMultiplier => 4f;
+        public override CombatDirector.EliteTierDef[] CanAppearInEliteTiers => EliteAPI.GetCombatDirectorEliteTiers().Where(x => x.eliteTypes.Contains(Addressables.LoadAssetAsync<EliteDef>("RoR2/Base/EliteFire/edFire.asset").WaitForCompletion())).ToArray();
+
+        public override void Init(ConfigFile config)
+        {
+            Backuped.Awake();
+            CreateConfig(config);
+            CreateLang();
+            CreateEquipment();
+            CreateEliteTiers();
+            CreateElite();
+            Hooks();
+        }
+
+        private void CreateConfig(ConfigFile config)
+        {
+
+        }
+
+        private void CreateEliteTiers()
+        {
+        }
+
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict();
+        }
+
+        public override void Hooks()
+        {
+            RecalculateStatsAPI.GetStatCoefficients += Inconsistent;
+            GlobalEventManager.onServerDamageDealt += OnDamage;
+        }
+
+        //If you want an on use effect, implement it here as you would with a normal equipment.
+        protected override bool ActivateEquipment(EquipmentSlot slot)
+        {
+            return false;
+        }
+
+        public void OnDamage(DamageReport report)
+        {
+            if (report.attacker && report.attackerBody && report.victim && report.victimBody)
+            {
+                if (report.attackerBody.equipmentSlot)
+                {
+                    CharacterBody body = report.attackerBody;
+                    CharacterBody victim = report.victimBody;
+                    if (body.HasBuff(EliteBuffDef))
+                    {
+                        float duration = 5.0f * report.damageInfo.procCoefficient;
+                        victim.AddTimedBuff(RoR2Content.Buffs.Entangle, duration, 1);
+                    }
+                }
+            }
+        }
+
+        public void Inconsistent(CharacterBody body, RecalculateStatsAPI.StatHookEventArgs args)
+        {
+            if (body.HasBuff(EliteBuffDef))
+            {
+                args.moveSpeedMultAdd += 0.7f;
+            }
+        }
+    }
+}
---
+++ GOTCE/Items/Lunar/VesselOfHeresy.cs
@@ -0,0 +1,94 @@
+﻿using BepInEx.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace GOTCE.Items.Lunar
+{
+    public class VesselOfHeresy : ItemBase<VesselOfHeresy>
+    {
+        public override string ConfigName => "Vessel Of Heresy";
+
+        public override string ItemName => "Vessel Of Heresy";
+
+        public override string ItemLangTokenName => "GOTCE_VesselOfHeresy";
+
+        public override string ItemPickupDesc => "Become Heretic.";
+
+        public override string ItemFullDescription => "On pickup, gain 1 stack each of Visions of Heresy, Hooks of Heresy, Strides of Heresy, and Essence of Heresy";
+
+        public override string ItemLore => "TBA";
+
+        public override ItemTier Tier => ItemTier.Lunar;
+
+        public override Enum[] ItemTags => new Enum[] { ItemTag.Damage, ItemTag.Utility, ItemTag.AIBlacklist, ItemTag.CannotSteal };
+
+        public override GameObject ItemModel => null;
+
+        public override Sprite ItemIcon => null;
+
+        public override void Init(ConfigFile config)
+        {
+            base.Init(config);
+        }
+
+        public override ItemDisplayRuleDict CreateItemDisplayRules()
+        {
+            return new ItemDisplayRuleDict(null);
+        }
+
+        public override void Hooks()
+        {
+            On.RoR2.Inventory.GiveItem_ItemIndex_int += Inventory_GiveItem_ItemIndex_int;
+            On.RoR2.Inventory.RemoveItem_ItemIndex_int += Inventory_RemoveItem_ItemIndex_int;
+        }
+
+        private void Inventory_RemoveItem_ItemIndex_int(On.RoR2.Inventory.orig_RemoveItem_ItemIndex_int orig, Inventory self, ItemIndex itemIndex, int count)
+        {
+            if (NetworkServer.active && itemIndex == Instance.ItemDef.itemIndex)
+            {
+                List<ItemDef> items = new()
+                {
+                    RoR2Content.Items.LunarPrimaryReplacement,
+                    RoR2Content.Items.LunarSecondaryReplacement,
+                    RoR2Content.Items.LunarUtilityReplacement,
+                    RoR2Content.Items.LunarSpecialReplacement
+                };
+
+                var stack = self.GetItemCount(itemIndex);
+                foreach (ItemDef itemDef in RoR2.ContentManagement.ContentManager._itemDefs)
+                {
+                    if (items.Contains(itemDef))
+                    {
+                        self.RemoveItem(itemDef, 1 * stack);
+                    }
+                }
+            }
+            orig(self, itemIndex, count);
+        }
+
+        private void Inventory_GiveItem_ItemIndex_int(On.RoR2.Inventory.orig_GiveItem_ItemIndex_int orig, Inventory self, ItemIndex itemIndex, int count)
+        {
+            orig(self, itemIndex, count);
+            if (NetworkServer.active && itemIndex == Instance.ItemDef.itemIndex)
+            {
+                List<ItemDef> items = new()
+                {
+                    RoR2Content.Items.LunarPrimaryReplacement,
+                    RoR2Content.Items.LunarSecondaryReplacement,
+                    RoR2Content.Items.LunarUtilityReplacement,
+                    RoR2Content.Items.LunarSpecialReplacement
+                };
+                var stack = self.GetItemCount(itemIndex);
+                foreach (ItemDef itemDef in RoR2.ContentManagement.ContentManager._itemDefs)
+                {
+                    if (items.Contains(itemDef))
+                    {
+                        self.GiveItem(itemDef, 1 * stack);
+                    }
+                }
+            }
+        }
+    }
+}
